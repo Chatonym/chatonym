d21bc65220a8581a2963a8d6168470aaca2fc4ba
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2024-05-04 19:31:34 +0200

    test: suppress logger in test env

---
+++ test/hooks.ts
@@ -0,0 +1,12 @@
+import { logger } from '../src/logger'
+
+const ENV = process.env.NODE_ENV
+const SHOULD_SILENT_LOGGER = ['test', undefined].includes(ENV)
+
+export const mochaHooks = {
+	beforeAll() {
+		if (SHOULD_SILENT_LOGGER) {
+			logger.transports.forEach((t) => t.silent = true)
+		}
+	},
+}
